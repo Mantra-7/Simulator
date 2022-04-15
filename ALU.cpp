68940f75d0a54501e5439f3bb888b651394bbccc
Mantra Trambadia <mantratrambadia1@gmail.com>
2022-04-15 19:28:34 +0530

    init
    Co-authored-by: Saptarshi-sen <Saptarshi-sen@users.noreply.github.com>

---
+++ ALU.cpp
@@ -0,0 +1,43 @@
+#include "Structures.hpp"
+
+int8 ALU::adder(int8 X , int8 Y, flag as)
+{
+    if(!as)
+    {
+        return X + Y;
+    }
+    else
+    {
+        return X - Y;
+    }
+}
+
+int8 ALU::MUL(int8 X, int8 Y)
+{
+    return X * Y;
+}
+
+int8 ALU::OR(int8 X, int8 Y)
+{
+    return X | Y;
+}
+
+int8 ALU::AND(int8 X, int8 Y)
+{
+    return X & Y;
+}
+
+int8 ALU::NOT(int8 X)
+{
+    return ~X;
+}
+
+int8 ALU::XOR(int8 X, int8 Y)
+{
+    return X ^ Y;
+}
+
+flag ALU::BNEQ(int X)
+{
+    return X == 0;
+}
